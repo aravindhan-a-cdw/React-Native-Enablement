5bb0801356af6d4e0d073372a25899236dcbc22f
Aravindhan A <aravindhan.asaithambi@cdw.com>
2024-02-06 12:07:56 +0530

    Made a reminder creating app

--- AnimationApp/index.js
+++ AnimationApp/index.js
@@ -1,10 +1,26 @@
 /**
- * @format
+ * @Author Aravindhan Asaithambi
  */
 
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
+import notifee, {EventType} from '@notifee/react-native';
+
+notifee.onBackgroundEvent(async ({type, detail}) => {
+  const {notification, pressAction} = detail;
+  console.log('onBackgroundEvent', type, pressAction, notification);
+  // Check if the user pressed the "Mark as read" action
+  if (type === EventType.ACTION_PRESS && pressAction.id === 'mark-as-read') {
+    // Update external API
+    await fetch(`https://demo123.free.beeceptor.com/todos`, {
+      method: 'POST',
+    });
+
+    // Remove the notification
+    await notifee.cancelNotification(notification.id);
+  }
+});
 
 AppRegistry.registerComponent(appName, () => App);
--- AnimationApp/src/screens/Notification.tsx
+++ AnimationApp/src/screens/Notification.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import notifee from '@notifee/react-native';
-import {Button, View} from 'react-native';
+import React, {useState} from 'react';
+import notifee, {TimestampTrigger, TriggerType} from '@notifee/react-native';
+import {Alert, Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import DatePicker from 'react-native-date-picker';
 
 const Notification = () => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+
   async function onDisplayNotification() {
     // Request permissions (required for iOS)
     await notifee.requestPermission();
@@ -27,11 +32,74 @@ const Notification = () => {
     });
   }
 
+  async function setReminder() {
+    const trigger: TimestampTrigger = {
+      type: TriggerType.TIMESTAMP,
+      timestamp: date.getTime(), // fire at 11:10am (10 minutes before meeting)
+    };
+
+    await notifee.createTriggerNotification(
+      {
+        title: 'Reminder',
+        body: message,
+        android: {
+          channelId: 'default',
+          smallIcon: 'ic_launcher',
+          pressAction: {
+            id: 'mark-as-read',
+          },
+        },
+        ios: {
+          sound: 'default',
+        },
+      },
+      trigger,
+    );
+
+    Alert.alert('Reminder Set', 'You will be reminded at the specified time');
+    setMessage('');
+  }
+
   return (
     <View>
-      <Button title="Display Notification" onPress={onDisplayNotification} />
+      <Text style={styles.title}>Reminder</Text>
+      <TextInput
+        onChangeText={(text: string) => setMessage(text)}
+        style={styles.textInput}
+        placeholder="Enter your message"
+        value={message}
+      />
+      <Button title="Choose Time" onPress={() => setOpen(true)} />
+
+      <DatePicker
+        modal
+        open={open}
+        date={date}
+        onConfirm={date => {
+          setOpen(false);
+          setDate(date);
+        }}
+        onCancel={() => {
+          setOpen(false);
+        }}
+      />
+      <Button title="Set Reminder" onPress={setReminder} />
+      <Button title="Display a Notification" onPress={onDisplayNotification} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginVertical: 20,
+  },
+});
 
 export default Notification;
