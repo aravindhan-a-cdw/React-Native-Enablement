4aa54f7837ee0c8d1160c940d7ad1ee4b5581965
Aravindhan A <aravindhan.asaithambi@cdw.com>
2024-02-07 23:22:16 +0530

    A base application created

---
+++ HealthTracker/src/App.tsx
@@ -0,0 +1,18 @@
+/**
+ *
+ * @Author: Aravindhan Asaithambi
+ *
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Health Tracker App</Text>
+    </View>
+  );
+};
+
+export default App;
