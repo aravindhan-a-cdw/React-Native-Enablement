eee7d24c3d73ef003ed61faa6f7dddc77793ade5
Aravindhan A <aravindhan.asaithambi@cdw.com>
2024-02-18 21:05:50 +0530

    Modified StepCalculator with lowpass filter algorithm

--- HealthTracker/src/components/StepCalculator.tsx
+++ HealthTracker/src/components/StepCalculator.tsx
@@ -7,8 +7,9 @@ import {
 import {useDispatch, useSelector} from 'react-redux';
 import {selectIsLoggingIn} from '../stores/slices/auth';
 import {addSteps} from '../stores/slices/data';
+import {lowpassFilter} from '../utils/filters';
 
-setUpdateIntervalForType(SensorTypes.accelerometer, 800); // in milliseconds
+setUpdateIntervalForType(SensorTypes.accelerometer, 400); // in milliseconds
 
 const StepCalculator = () => {
   const isLoggedIn = useSelector(selectIsLoggingIn);
@@ -18,11 +19,43 @@ const StepCalculator = () => {
     if (!isLoggedIn) {
       return;
     }
-    const subscription = accelerometer.subscribe(({x, y, z, timestamp}) => {
-      console.log({x, y, z, timestamp});
-      if (x > 1.5 || z > 1.5) {
+    let lastMagnitude = 0;
+    let stepCount = 0;
+
+    const threshold = 0.2;
+    const filterFactor = 0.1;
+
+    const subscription = accelerometer.subscribe(({x, y, z}) => {
+      console.log(
+        'X:',
+        Math.abs(x) > 7 ? 0 : x,
+        'Y:',
+        Math.abs(y) > 7 ? 0 : y,
+        'Z:',
+        Math.abs(z) > 7 ? 0 : z,
+      );
+      x = Math.abs(x) > 7 ? 0 : x;
+      y = Math.abs(y) > 7 ? 0 : y;
+      z = Math.abs(z) > 7 ? 0 : z;
+      const magnitude = Math.sqrt(x * x + y * y + z * z);
+      const filteredMagnitude = lowpassFilter(
+        magnitude,
+        lastMagnitude,
+        filterFactor,
+      );
+
+      console.log('Magnitude:', filteredMagnitude);
+      console.log('Difference', Math.abs(filteredMagnitude - lastMagnitude));
+
+      if (
+        Math.abs(filteredMagnitude - lastMagnitude) > threshold &&
+        filteredMagnitude > lastMagnitude
+      ) {
+        console.log('Step detected', stepCount);
+        stepCount++;
         dispatch(addSteps(1));
       }
+      lastMagnitude = filteredMagnitude;
     });
     return () => {
       subscription.unsubscribe();
