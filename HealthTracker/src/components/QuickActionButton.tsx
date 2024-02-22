100730d9b29f673edabebeb168e0204e5c81f75a
Aravindhan A <aravindhan.asaithambi@cdw.com>
2024-02-22 23:29:05 +0530

    Modified QuickActionButton to solve issue in ios

--- HealthTracker/src/components/QuickActionButton.tsx
+++ HealthTracker/src/components/QuickActionButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import ActionButton from 'react-native-action-button';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {useDispatch} from 'react-redux';
@@ -9,29 +9,24 @@ const QuickActionButton = () => {
   const dispatch = useDispatch();
 
   return (
-    <View style={styles.container}>
-      <ActionButton style={styles.actionButton} buttonColor="rgba(231,76,60,1)">
-        <ActionButton.Item
-          buttonColor="#9b59b6"
-          title="Add 250ml"
-          onPress={() => dispatch(addWater({data: 250}))}>
-          <MaterialIcon name="cup-water" style={styles.actionButtonIcon} />
-        </ActionButton.Item>
-        <ActionButton.Item
-          buttonColor="#3498db"
-          title="Add 500ml"
-          onPress={() => dispatch(addWater({data: 500}))}>
-          <MaterialIcon name="glass-mug" style={styles.actionButtonIcon} />
-        </ActionButton.Item>
-      </ActionButton>
-    </View>
+    <ActionButton style={styles.actionButton} buttonColor="rgba(231,76,60,1)">
+      <ActionButton.Item
+        buttonColor="#9b59b6"
+        title="Add 250ml"
+        onPress={() => dispatch(addWater({data: 250}))}>
+        <MaterialIcon name="cup-water" style={styles.actionButtonIcon} />
+      </ActionButton.Item>
+      <ActionButton.Item
+        buttonColor="#3498db"
+        title="Add 500ml"
+        onPress={() => dispatch(addWater({data: 500}))}>
+        <MaterialIcon name="glass-mug" style={styles.actionButtonIcon} />
+      </ActionButton.Item>
+    </ActionButton>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-  },
   actionButtonIcon: {
     fontSize: 20,
     height: 20,
