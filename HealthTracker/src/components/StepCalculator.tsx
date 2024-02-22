a070c2a8d896734c5ca44dcc46007738de72dacb
Aravindhan A <aravindhan.asaithambi@cdw.com>
2024-02-22 23:30:01 +0530

    Handled error in sensor observer

--- HealthTracker/src/components/StepCalculator.tsx
+++ HealthTracker/src/components/StepCalculator.tsx
@@ -8,6 +8,7 @@ import {useDispatch, useSelector} from 'react-redux';
 import {selectIsLoggingIn} from '../stores/slices/auth';
 import {addSteps, saveData} from '../stores/slices/data';
 import {lowpassFilter} from '../utils/filters';
+import {Alert} from 'react-native';
 
 setUpdateIntervalForType(SensorTypes.accelerometer, 400); // in milliseconds
 
@@ -25,31 +26,46 @@ const StepCalculator = () => {
     const threshold = 0.2;
     const filterFactor = 0.1;
 
-    const subscription = accelerometer.subscribe(({x, y, z}) => {
-      x = Math.abs(x) > 7 ? 0 : x;
-      y = Math.abs(y) > 7 ? 0 : y;
-      z = Math.abs(z) > 7 ? 0 : z;
-      const magnitude = Math.sqrt(x * x + y * y + z * z);
-      const filteredMagnitude = lowpassFilter(
-        magnitude,
-        lastMagnitude,
-        filterFactor,
-      );
-
-      if (
-        Math.abs(filteredMagnitude - lastMagnitude) > threshold &&
-        filteredMagnitude > lastMagnitude
-      ) {
-        console.log('Step detected', stepCount);
-        stepCount++;
-        dispatch(addSteps({data: 1}));
-        if (stepCount % 10 === 0) {
-          console.debug('Saving Data');
-          dispatch(saveData());
+    // if (!accelerometer.isSensorAvailable) {
+    //   Alert.alert(
+    //     'Sensor not available',
+    //     'Accelerometer is not available on this device',
+    //   );
+    //   return;
+    // }
+    const subscription = accelerometer.subscribe(
+      ({x, y, z}) => {
+        x = Math.abs(x) > 7 ? 0 : x;
+        y = Math.abs(y) > 7 ? 0 : y;
+        z = Math.abs(z) > 7 ? 0 : z;
+        const magnitude = Math.sqrt(x * x + y * y + z * z);
+        const filteredMagnitude = lowpassFilter(
+          magnitude,
+          lastMagnitude,
+          filterFactor,
+        );
+        if (
+          Math.abs(filteredMagnitude - lastMagnitude) > threshold &&
+          filteredMagnitude > lastMagnitude
+        ) {
+          console.log('Step detected', stepCount);
+          stepCount++;
+          dispatch(addSteps({data: 1}));
+          if (stepCount % 10 === 0) {
+            console.debug('Saving Data');
+            dispatch(saveData());
+          }
         }
-      }
-      lastMagnitude = filteredMagnitude;
-    });
+        lastMagnitude = filteredMagnitude;
+      },
+      _ => {
+        // console.error('The sensor is not available', error);
+        Alert.alert(
+          'Sensor not available',
+          'Accelerometer is not available on this device',
+        );
+      },
+    );
     return () => {
       subscription.unsubscribe();
     };
