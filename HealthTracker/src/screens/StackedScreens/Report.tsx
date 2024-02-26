826f2fbd9469ca7c23a39ff33bfa248e7dd6ac66
Aravindhan A <aravindhan.asaithambi@cdw.com>
2024-02-26 11:59:59 +0530

    Modified reports data order

--- HealthTracker/src/screens/StackedScreens/Report.tsx
+++ HealthTracker/src/screens/StackedScreens/Report.tsx
@@ -5,46 +5,25 @@ import StatusChart from '../../components/StatusChart';
 import {WeeklyReportConstants} from '../../constants/pageConstants';
 
 const Report = () => {
-  // const stepData = [
-  //   {value: 2000, label: '02-16'},
-  //   {value: 3460, label: '02-17'},
-  //   {value: 1856, label: '02-18'},
-  //   {value: 4003, label: '02-19'},
-  //   {value: 3687, label: '02-20'},
-  //   {value: 6003, label: '02-21'},
-  //   {value: 5456, label: '02-22'},
-  //   {value: 1203, label: '02-23'},
-  // ];
-  // const waterData = [
-  //   {value: 4000, label: '02-16'},
-  //   {value: 8460, label: '02-17'},
-  //   {value: 6856, label: '02-18'},
-  //   {value: 5003, label: '02-19'},
-  //   {value: 3687, label: '02-20'},
-  //   {value: 6003, label: '02-21'},
-  //   {value: 7456, label: '02-22'},
-  //   {value: 5203, label: '02-23'},
-  // ];
-
   const stepData = [
-    {value: 2000, label: 'Sun'},
-    {value: 3460, label: 'Mon'},
+    {value: 1203, label: 'Mon'},
     {value: 1856, label: 'Tue'},
     {value: 4003, label: 'Wed'},
     {value: 3687, label: 'Thu'},
     {value: 6003, label: 'Fri'},
     {value: 5456, label: 'Sat'},
-    {value: 1203, label: 'Sun'},
+    {value: 2000, label: 'Sun'},
+    {value: 3460, label: 'Mon'},
   ];
   const waterData = [
-    {value: 4000, label: 'Sun'},
-    {value: 8460, label: 'Mon'},
+    {value: 5203, label: 'Mon'},
     {value: 6856, label: 'Tue'},
     {value: 5003, label: 'Wed'},
     {value: 3687, label: 'Thu'},
     {value: 6003, label: 'Fri'},
     {value: 7456, label: 'Sat'},
-    {value: 5203, label: 'Sun'},
+    {value: 4000, label: 'Sun'},
+    {value: 8460, label: 'Mon'},
   ];
 
   return (
